7978de0d9c004d41579c99e9f81053d6b843b95d
sondonghee123 <ghost1229@naver.com>
2024-07-08 11:37:04 +0900

    feat : 경매 소켓 확인 (2024/07/08, 11:37)

--- src/main/java/com/shinhan/heehee/controller/MainController.java
+++ src/main/java/com/shinhan/heehee/controller/MainController.java
@@ -1,17 +1,25 @@
 package com.shinhan.heehee.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.shinhan.heehee.dto.request.BanUserDTO;
 import com.shinhan.heehee.dto.response.CategoryDTO;
+import com.shinhan.heehee.dto.response.ElasticSyncDTO;
 import com.shinhan.heehee.service.AlarmService;
 import com.shinhan.heehee.service.AuctionService;
+import com.shinhan.heehee.service.ElasticsearchService;
 import com.shinhan.heehee.service.MainService;
+import com.shinhan.heehee.service.UserService;
 
 @Controller
 public class MainController {
@@ -24,6 +32,12 @@ public class MainController {
 	
 	@Autowired
 	AuctionService auctionService;
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	ElasticsearchService elasticsearchService;
 	
 	@GetMapping("/main")
 	public String main(Model model, Principal principal) {
@@ -44,8 +58,17 @@ public class MainController {
 	
 	
 	@GetMapping("/main/search")
-	public String search(Model model, Principal principal) {
-		model.addAttribute("rankProdList", mainservice.rankProdList());
+	public String search(@RequestParam(value="keyword", required = false) String keyword
+						,@RequestParam(value="category", required = false) Integer cateNum
+						, Model model, Principal principal) {
+		
+		List<ElasticSyncDTO> productArr = new ArrayList<>();
+		
+		if(keyword != null) productArr = elasticsearchService.searchKeyword(keyword);
+		if(cateNum != null) productArr = elasticsearchService.searchCategory(cateNum);
+		
+		model.addAttribute("productArr", productArr);
+		model.addAttribute("resultCount", productArr.size());
 		
 		String loginId = (principal != null) ? principal.getName() : "admin";
 		int alarmCount = alarmService.alarmCount(loginId);
@@ -55,5 +78,18 @@ public class MainController {
 		model.addAttribute("mainCateList", mainCateList); // header 카테고리
 		
 		return "/main/search";
+	}
+	
+	@GetMapping("/ban")
+	public String ban(@RequestParam String userId, Model model) {
+		BanUserDTO banUserDTO = userService.banCheck(userId);
+		model.addAttribute("banUserDto", banUserDTO);
+		return "/common/banUser";
+	}
+	
+	@GetMapping("/bank")
+	@ResponseBody
+	public List<Map<String,Object>> getBankKind() {
+		return userService.getBankKind();
 	}
 }
